d7d715ffa37504f91aa0afeb92a8852790314649
Zain Ali <s.zaink5@gmail.com>
2026-03-02 16:09:47 +0400

    Add JSON parsing error in uploading csv

--- backend/main.py
+++ backend/main.py
@@ -14,11 +14,35 @@ def dataframe_to_json_records(df: pd.DataFrame, limit: int = 5):
 
 @app.post("/upload_csv/")
 async def upload_csv(file: UploadFile = File(...)):
-    # Read CSV into pandas
-    df = pd.read_csv(file.file)
-    
-    # Store in DuckDB
-    db.execute("CREATE OR REPLACE TABLE data AS SELECT * FROM df")
+    try:
 
-    # Return first 5 rows as JSON
-    return JSONResponse(content=dataframe_to_json_records(df))
+        # Read CSV into pandas
+        df = pd.read_csv(file.file)
+        
+        # Store in DuckDB
+        db.execute("CREATE OR REPLACE TABLE data AS SELECT * FROM df")
+
+        # Confirm storage by querying rwo count
+        row_count = db.execute("SELECT (*) FROM data").fetchall()[0][0]
+        col_count = len(df.columns)
+        print(f"Rows stored in DuckDB: {row_count}")
+        
+
+        # Return first 5 rows as JSON
+        return JSONResponse(content={
+            "success": True,
+            "message": f"File uploaded successfully",
+            "rows": row_count,
+            "columns": col_count,
+            "column_names": list(df.columns),
+            "sample_data": dataframe_to_json_records(df) # df.head(5).to_dict(orient="records")            
+            })
+
+    except Exception as e:
+        return JSONResponse(
+            status_code=400,
+            content={
+                "success": False,
+                "message": f"Error uploading file: {str(e)}"
+            }
+        )
