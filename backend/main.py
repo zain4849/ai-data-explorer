eb213100dc1f0a66f731c383f58d141cbaffd97d
Zain Ali <s.zaink5@gmail.com>
2026-03-03 16:06:01 +0400

    Implement logic for /query endpoint

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI, UploadFile, File, Query
 from fastapi.responses import JSONResponse
+
 from .db import db
+from .llm import generate_sql, generate_insights
+from .charting import generate_chart
+
 import pandas as pd
 
 app = FastAPI()
@@ -20,5 +24,19 @@ async def upload_csv(file: UploadFile = File(...)):
 @app.get("/query/")
 async def query_data(nl_query: str = Query(...)):
     # http://localhost:8000/query?nl_query=top%2010%20customers
-    pass
+    sql = generate_sql(nl_query)
 
+    try:
+        df = db.query(sql) # df contains the results of the SQL query as a pandas DataFrame
+
+        insights = generate_insights(df.head(10).to_string()) # Generate insights based on a preview of the data
+        chart_html = generate_chart(df)
+
+        return {
+            "sql": sql,
+            "data_preview": dataframe_to_json_records(df, 50),
+            "insights": insights,
+            "chart_html": chart_html
+        }
+    except Exception as e:
+        return JSONResponse(status_code=400, content={"error": str(e), "sql": sql})
