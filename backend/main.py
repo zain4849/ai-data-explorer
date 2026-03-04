12880684a75211b8aa9fcce29c7a0731c6c43a96
Zain Ali <s.zaink5@gmail.com>
2026-03-04 21:26:44 +0400

    Remove trailing slashes for backend endpoints for consistency w/ frontend

--- backend/main.py
+++ backend/main.py
@@ -17,13 +17,19 @@ def dataframe_to_json_records(df: pd.DataFrame, limit: int = 5):
     safe_df = df.head(limit).astype(object).where(pd.notna(df.head(limit)), None)
     return safe_df.to_dict(orient="records")
 
-@app.post("/upload_csv/")
+@app.post("/upload_csv")
 async def upload_csv(file: UploadFile = File(...)):
     df = pd.read_csv(file.file) # Read CSV into pandas
     db.load_dataframe(df) # After this DuckDB has a table named data w/ contents of df
     return dataframe_to_json_records(df) # Return first 5 rows as JSON records for confirmation to client
+    '''
+    [
+        {"Name": "John", "Age": 25, "City": "New York"},
+        {"Name": "Alice", "Age": 30, "City": "Paris"}
+    ]
+    '''
 
-@app.get("/query/")
+@app.get("/query")
 async def query_data(nl_query: str = Query(...)):
     # http://localhost:8000/query?nl_query=top%2010%20customers
 
